df12946300934059e7f44a8e49c82f04cb60038e
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-03-22 00:51:11 +0200

    Refactor: Refactored hooks and types into their own file

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,43 +1,11 @@
 "use client";
 
-import { redirect } from "next/navigation";
 import darkModeBackground from "../../public/dark-mode-background.jpg";
-import { getSession, signOut } from "../lib/actions/auth-actions";
-import { auth } from "../lib/auth";
-import { useActionState, useEffect, useState } from "react";
-
-interface SessionData {
-  session: {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
-    userId: string;
-    expiresAt: Date;
-    token: string;
-    ipAddress?: string | null | undefined;
-    userAgent?: string | null | undefined;
-  };
-  user: {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
-    email: string;
-    emailVerified: boolean;
-    name: string;
-    image?: string | null | undefined;
-  };
-}
+import { signOut } from "../lib/actions/auth-actions";
+import { useSession } from "../lib/hooks";
 
 export default function DashboardPage() {
-  const [session, setSession] = useState<SessionData | null>(null);
-  useEffect(() => {
-    const curSession = async () => {
-      const data = await getSession();
-      setSession(data);
-    };
-
-    curSession();
-  }, []);
+  const session = useSession()
   console.log(session);
   return (
     <main className="text-white">
